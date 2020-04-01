534c1e3c09f65f172b75ca7abad38c3d779d0ce9
liyaSileshi <liyasileshi@gmail.com>
2020-03-31 23:02:34 -0700

    game works

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Square from './Square'
+import {Square, calculateWinner} from './Square'
 import './Board.css'
 class Board extends React.Component {
     constructor(props) {
@@ -12,7 +12,10 @@ class Board extends React.Component {
 
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         squares: squares,
         xIsNext: !this.state.xIsNext,
@@ -29,9 +32,15 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + 
-        (this.state.xIsNext ? 'X' : 'O');
-    
+        const winner = 
+        calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+          status = 'Winner: ' + winner;
+        } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X': 'O');
+        }
+
         return (
           <div>
             <div className="status">{status}</div>
